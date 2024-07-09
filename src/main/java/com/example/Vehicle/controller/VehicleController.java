b984bdd38b1115ec0c28818b45acd3d4dda94932
hon2838 <50094669+hon2838@users.noreply.github.com>
2024-07-10 01:32:00 +0800

    update for to fix cors error

--- src/main/java/com/example/Vehicle/controller/VehicleController.java
+++ src/main/java/com/example/Vehicle/controller/VehicleController.java
@@ -3,6 +3,7 @@ package com.example.Vehicle.controller;
 import com.example.Vehicle.entity.Vehicle;
 import com.example.Vehicle.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/vehicles")
+@CrossOrigin(origins = "*")
 public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
